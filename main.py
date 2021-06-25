6fc8f2b153cfe142be03ee2bd605438d10799303
SHWETA <74524265+shwe0601@users.noreply.github.com>
2021-06-25 12:33:27 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,45 @@
+import requests
+import os
+from twilio.rest import Client
+from twilio.http.http_client import TwilioHttpClient
+
+OWN_ENDPOINT="https://api.openweathermap.org/data/2.5/onecall"
+api_key=os.environ.get("OWN_API_KEYS")
+account_sid = os.environ.get("OWN_API_KEY")
+auth_token = os.environ.get("OWN_API_TOKEN")
+# these are personal aoi keys if you run as it is u will get error
+# make sure you sign in to twilio and get your own API keys
+#the above variables are environmental variables (used OS) which means it wont come for  everyone.
+
+weather_params={
+    "lat":25.611219,
+    "lon":85.130692,
+    "appid":api_key,
+    "exclude":"current,minutely,daily"
+}
+
+response=requests.get(OWN_ENDPOINT,params=weather_params)
+response.raise_for_status()
+weather_data=response.json()
+weather_slicing=weather_data["hourly"][:12]
+will_rain=False
+for hour in weather_slicing:
+    condition_data=hour["weather"][0]["id"]
+    if int(condition_data)<700:
+        will_rain=True
+
+if will_rain:
+    proxy_client = TwilioHttpClient()
+    proxy_client.session.proxies = {'https': os.environ['https_proxy']}
+
+    client = Client(account_sid, auth_token,http_client=proxy_client)
+    message = client.messages \
+        .create(
+        body="Its going to rain today. Take Umbrella ☔",
+        from_= #after signong in to twilaio u will get your own number,
+        to= #give your verified number used in twilio.
+    )
+    print(message.status)
+
+
+
